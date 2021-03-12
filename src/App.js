bfc2899405b1ffd362d5df5ffb6505fba3ccb0e7
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-03-12 11:19:24 +0100

    OVerview page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Overview from "./Components/Overview/MainPart/Overview";
+import "antd";
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div>
+      <Overview />
+    </div>
+  );
+}
+
+export default App;
